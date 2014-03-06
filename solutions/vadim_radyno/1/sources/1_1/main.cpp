655f6fb752a3c038159815898e49399e46628474
vadim <vadimradyno@gmail.com>
2014-03-06 21:07:14 +0300

    -

--- solutions/vadim_radyno/1/sources/1_1/main.cpp
+++ solutions/vadim_radyno/1/sources/1_1/main.cpp
@@ -1,73 +1,33 @@
 #include <iostream>
-
-void f1() 
+#include <fstream>
+#include <string>
+using namespace std;
+namespace Paths
 {
-	std::cout << "Empty function" << std::endl;
-}
-void f2( int i ) 
-{
-	i = 2;
-	std::cout << i << std::endl;
-}
-void f3( const int i ) 
-{
-	// i = 3;
-}
-void f4( int& i ) 
-{
-	i = 4;
-	std::cout << i << std::endl;
-}
-void f5( const int& i ) 
-{
-	//i = 5;
-}
-void f6( int* i ) 
-{
-	*i = 6;
-	delete i;
-	i = new int ( 66 );
-	std::cout << *i << std::endl;
-}
-void f7( const int* i ) 
-{
-	// *i = 7; error
-	delete i;
-	i = new int ( 77 );
-	std::cout << *i << std::endl;
-}
-void f8( int * const i ) 
-{
-	*i = 8;
-	/*delete i; error
-	i = new int ( 88 ); */
-	std::cout << *i << std::endl;
-}
-void f9( const int* const i ) 
-{
-	/**i = 9;
-	delete i; error
-	i = new int ( 99 ); */
-	
+    const string input_file = SOURCE_DIR "/sources/1_1/input.txt";
+    const string output_file = SOURCE_DIR "/sources/1_1/output.txt";
 }
 
-int main( void )
+int main( int argc, char* argv[] )
 {
-	f1();	
-	f2( 1 );
-	f3( 1 );
-	
+    std::ifstream input_file( Paths::input_file );
+    
 
-	// f4( 1 );
-	int i = 1;
-	f4( i );
+    if ( !input_file.eof() )
+    {
+        std::string first_line;
+        std::getline( input_file, first_line );
 
-	int* i_ptr = new int( 3 );
-	f6( i_ptr );
-	f7( i_ptr );
-	f8( i_ptr );
+        while ( !input_file.eof() )
+        {
+            input_file >> first_line;
 
-	delete i_ptr;
+            std::getline( input_file, first_line );
+            std::cout << first_line.c_str() << std::endl;
+        }
 
-	return 0;
+    }
+
+
+    input_file.close();
 }
