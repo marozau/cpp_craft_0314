ea5949ccbbab59a360254fa0c212cf632a37971f
kurman.vladimir <kurman.vladmir@gmail.com>
2014-03-08 11:22:48 +0300

    Homework #1.

---
+++ solutions/kurman_vladimir/1/sources/1_1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void prepareString(wstring & str)
+{
+	str.erase(remove_if(str.begin(), str.end(), [](wchar_t ch)
+	{
+		return ch == ' ' || ch == '\\' || ch == '-';
+	}), str.end());
+	for (wchar_t & ch : str)
+		ch = tolower(ch);
+}
+
+void getlineFromFile(wifstream & is, wstring & str)
+{
+	getline(is, str);
+	prepareString(str);
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "");
+	wifstream fin(SOURCE_DIR "/input.txt");
+	ofstream fout(BINARY_DIR "/output.txt");
+	if (!fin.is_open())
+	{
+		cout << "Something is wrong with in/out files\n";
+		return 1;
+	}
+	wstring mainStr;
+	getlineFromFile(fin, mainStr);
+	while (!fin.eof())
+	{
+		wstring str;
+		getlineFromFile(fin, str);
+		if (!str.empty())
+			fout << (mainStr.find(str) == wstring::npos ? "NO" : "YES") << endl;
+	}
+	fin.close();
+	fout.close();
+	return 0;
+}
