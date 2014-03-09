2eeec446f71af73ab5be7375d522c36a79dbc782
Alexei Romanoff <drednout.by@gmail.com>
2014-03-09 21:13:19 +0300

    Problem 2 of week 1 has been solved

---
+++ solutions/alexei_romanoff/sources/week1_rational_numbers/rational_numbers.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+
+
+int main(int argc, char **argv) {
+    std::ifstream input;
+    std::ofstream output;
+
+    //some scaffolds
+    //std::cerr << "DEBUG: SOURCE_DIR is " << SOURCE_DIR << "\n";
+    //std::cerr << "DEBUG: BINARY_DIR is " << BINARY_DIR << "\n";
+
+    input.open(SOURCE_DIR "/sources/week1_rational_numbers/input.txt");
+    output.open(BINARY_DIR "/output.txt");
+    if (not input.is_open()) {
+        std::cerr << "Unable to open the input file. Reason: " << 
+                      strerror(errno) << "\n";
+        return 1;
+    }
+    if (not output.is_open()) {
+        std::cerr << "Unable to open the output file. Reason: " << 
+                      strerror(errno) << "\n";
+        return 1;
+    }
+
+    unsigned int N;
+    std::set<std::string> codes;
+    std::string line;
+
+    std::getline(input, line);
+    std::istringstream(line) >> N;
+    //std::cerr << "DEBUG: N is " << N << "\n";
+    
+    unsigned int line_count = 0;
+    double next_value = 0.0;
+    std::string next_str_value;
+    std::set<std::string>::iterator it;
+
+    while ( std::getline(input, line) ) {
+        next_value = ::strtod(line.c_str(), 0);
+        std::ostringstream ss;
+        ss  << std::setprecision(5) << ::trunc(next_value * 10000) / 10000;
+        next_str_value = ss.str();
+
+        //some scaffolds
+        //std::cerr << "DEBUG: next_str_value is " << next_str_value << "\n";
+        //std::cerr << "DEBUG: line_count is " << line_count << "\n";
+        if (line_count < N) {
+            codes.insert(next_str_value);
+            ++line_count;
+            continue;
+        }
+        it = codes.find(next_str_value);
+        if (it == codes.end()) {
+            output << "NO" << "\n";
+        } else {
+            output << "YES" << "\n";
+        }
+        
+        ++line_count;
+    }
+
+    input.close();
+    output.close();
+
+
+    return 0;
+}
