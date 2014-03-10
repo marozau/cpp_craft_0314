952339ba470f4d8e68a499f0d556c9c4cc94b3bb
TereschenkovaEugeniya <zhenya.tereschenkova@gmail.com>
2014-03-11 02:17:06 +0300

    commit first version puzzles

--- solutions/eugeniya_tereschenkova/1/sources/1_1/main.cpp
+++ solutions/eugeniya_tereschenkova/1/sources/1_1/main.cpp
@@ -1,5 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <algorithm>
+using namespace std;
 
-int main( void )
+
+namespace Constants_1_1
 {
+	const char* input_file = SOURCE_DIR "/tests/1_1Input.txt";
+	const char* output_file = SOURCE_DIR "/tests/1_1Output.txt";
+	const string good = "YES";
+	const string bad  = "NO";
+	const char* Locale = "Russian";
+	const size_t maxInputLen = 100000;
+}
+
+bool isSpaceSymb(const char& symb)
+{
+	switch(symb)
+	{
+	case ' ':
+	case '-':
+	case '\\':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+bool formatting_line(ifstream& stream, string& line)
+{
+	bool bReturn = true;
+	getline(stream, line);
+	if(line.empty())
+		return bReturn;
+	if(line.size() > Constants_1_1::maxInputLen)
+		cout << "Could not open input file" << endl;
+	line.erase(remove_if(line.begin(), line.end(), isSpaceSymb), line.end());
+	transform(line.begin(), line.end(), line.begin(), tolower);
+	return bReturn;
+}
+
+int main()
+{
+	setlocale(LC_ALL, Constants_1_1::Locale);
+	ifstream input_file( Constants_1_1::input_file );
+	string line, key;
+	if(!input_file.is_open())
+	{
+		cout << "Could not open input file" << endl;
+		return 1;
+	}
+	if(!input_file.eof())
+	{
+		if(!formatting_line(input_file, line))
+		{
+			input_file.close();
+			return 1;
+		}
+		ofstream output_file( Constants_1_1::output_file );
+		if(!output_file.is_open())
+		{
+			cout << "Could not open output file" << endl;
+			input_file.close();
+			return 1;
+		}
+
+		while(!input_file.eof())
+		{
+			formatting_line(input_file, key);
+			if(key.empty())
+				continue;
+			reverse(key.begin(), key.end());
+			
+			(line.find(key) != string::npos) ? 
+				output_file << Constants_1_1::good << endl	:
+				output_file << Constants_1_1::bad << endl;
+		}
+
+		input_file.close();
+		output_file.close();
+	}
+
 	return 0;
 }
--- solutions/eugeniya_tereschenkova/1/sources/1_2/main.cpp
+++ solutions/eugeniya_tereschenkova/1/sources/1_2/main.cpp
@@ -1,5 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
-int main( void )
+namespace Constants_1_2
 {
+	const char* input_file = SOURCE_DIR "/tests/1_2Input.txt";
+	const char* output_file = SOURCE_DIR "/tests/1_2Output.txt";
+	const string good = "YES";
+	const string bad  = "NO";
+	const char* Locale = "Russian";
+	const int nPrecisionNumbers = 10000;
+}
+
+void get_Codes(ifstream& stream, vector<long long int>& vectCodes)
+{
+	int nCodesCount = 0, nI = 0;
+	double Passw;
+
+	stream >> nCodesCount;
+	if(stream.eof())
+		return;
+
+	if(nCodesCount <= 0 || nCodesCount > 1000000)
+		return;
+	else
+		vectCodes.reserve(nCodesCount);
+
+	for( nI = 0; nI < nCodesCount && !stream.eof(); nI++) 
+	{
+		stream >> Passw;
+		vectCodes.push_back((long long int)(Passw*Constants_1_2::nPrecisionNumbers));
+	}
+	sort(vectCodes.begin(), vectCodes.end());
+}
+
+bool compare_CodesAndPasswords(ifstream& stream, const vector<long long int>& vectCodes) 
+{
+	ofstream output_file( Constants_1_2::output_file );
+	bool bReturn = true;
+	double Passw;
+	if(!output_file.is_open())
+	{
+		cout << "Could not open output file" << endl;
+		return 1;
+	}
+	while(!stream.eof())
+	{
+		stream >> Passw;     
+
+		if(binary_search(vectCodes.begin(), vectCodes.end(), (long long int)(Passw*Constants_1_2::nPrecisionNumbers)))
+			output_file << Constants_1_2::good << endl;
+		else 
+			output_file << Constants_1_2::bad << endl;
+	}
+	output_file.close();
+	return bReturn;
+}
+
+int main()
+{
+	setlocale(LC_ALL, Constants_1_2::Locale);
+	ifstream input_file( Constants_1_2::input_file );
+	vector<long long int> vectCodes;
+
+	if(!input_file.is_open())
+	{
+		cout << "Could not open input file" << endl;
+		return 1;
+	} 
+	get_Codes(input_file, vectCodes);
+
+	if(!compare_CodesAndPasswords(input_file, vectCodes))
+		return 1;
+	input_file.close();
+
 	return 0;
 }
