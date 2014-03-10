2345aa96984a7facd25ed4e8ed80f907828f0f5f
Dmitry Senkovich <dmitry.senkovich2013@yandex.ru>
2014-03-11 01:22:36 +0300

    my folder is created

--- solutions/dmitriy_senkovich/1/sources/solution_1_5/solution_1_5.cpp
+++ solutions/dmitriy_senkovich/1/sources/solution_1_5/solution_1_5.cpp
@@ -6,40 +6,59 @@
 
 using namespace std;
 
-struct point{
-	int x,y;
-public:
-	point(){ x=0; y=0; }
+void if_land( vector<string>& a, int n, int m, queue<vector<int>>& tabl, vector<int> curr, int side ){
 
-	point(int x, int y){ 
-		point::x=x;
-		point::y=y;
+	switch( side ){
+	case 0:{
+		if ( ( curr[0]-1 < 0 ) )
+			return;
+		if ( a[curr[0]-1][curr[1]] != 'o' )
+			return;
+		else{
+			curr[0] = curr[0]-1;
+			tabl.push( curr );
+			a[curr[0]][curr[1]] = '~';
+		}
+		   }
+		   break;
+	case 1:{
+		if ( ( curr[1]-1 < 0 ) )
+			return;
+		if ( a[curr[0]][curr[1]-1] != 'o' )
+			return;
+		else{
+			curr[1] = curr[1] - 1;
+ 			tabl.push( curr );
+			a[curr[0]][curr[1]] = '~';
+		}
+		   }
+		   break;
+	case 2:{
+		if ( ( curr[0]+1 > n-1 ) )
+			return;
+		if ( a[curr[0]+1][curr[1]] != 'o' )
+			return;
+		else{
+			curr[0] = curr[0]+1;
+			tabl.push( curr );
+			a[curr[0]][curr[1]] = '~';
+		}
+		   }
+		   break;
+	case 3:{
+		if ( ( curr[1]+1 > m-1 ) )
+			return;
+		if ( a[curr[0]][curr[1]+1] != 'o' )
+			return;
+		else{
+			curr[1] = curr[1]+1;
+			tabl.push( curr );
+			a[curr[0]][curr[1]] = '~';
+		}
+		   }
+		   break;
 	}
 
-	point up(){
-		return point(x,y-1);
-	}
-
-	point down(){
-		return point(x,y+1);
-	}
-
-	point left(){
-		return point(x-1,y);
-	}
-
-	point right(){
-		return point(x+1,y);
-	}
-};
-
-void side(vector<string>& a, int n, int m, queue<point>& q, point& p){
-	if(p.x<0||p.y<0||p.x>=n||p.y>=m)
-		return;
-	if(a[p.x][p.y]=='~')
-		return;
-	q.push( p );
-	a[p.x][p.y]='~';
 }
 
 int main(){
@@ -47,59 +66,48 @@ int main(){
 	ifstream in;
 	ofstream out;
 
-	in.open(BINARY_DIR"/input.txt",ios_base::in);
-	out.open(BINARY_DIR"/output.txt",ios_base::out);
+	in.open( BINARY_DIR"/input.txt" );
+	out.open( BINARY_DIR"/output.txt" );
 
-	if(!in.is_open()||!out.is_open()){
+	if( (!in.is_open()) || (!out.is_open()) ){
 		cerr<<"Error while opening files\n";
 		return 1;
 	}
 
 	vector<string> a;
-	int n,m,ans=0;
-	string buf;
+	int n, m;
+	int count = 0;
+	string buff_str;
 
-	try{
-		while(!in.eof()){
-			if(!(in>>buf))
-				throw exception("Error while reading data\n");
-			a.push_back(buf);
-		}
-	} 
-
-	catch(exception& e){
-		cerr<<e.what();
-		return 1;
+	while( !in.eof() ){
+		in >> buff_str;
+		a.push_back( buff_str );
 	}
 
-	n=(int)a.size();
-	m=(int)a[0].size();
+	n = (int)a.size();
+	m = (int)a[0].size();
 
-	for(int i=0; i<n; i++)
-		for(int j=0; j<m; j++)
-			if(a[i][j]=='o'){
-				a[i][j]='~';
-				queue<point> q;
-				q.push(point(i,j));
-				while(!q.empty()){
-					point start = q.front();
-					q.pop();
-					side(a, n, m, q, start.up());
-					side(a, n, m, q, start.down());
-					side(a, n, m, q, start.left());
-					side(a, n, m, q, start.right());
-					}
-				ans++;
+	for( int i = 0; i < n; i++ )
+		for( int j = 0; j < m; j++ )
+			if( a[i][j] == 'o' ){
+				a[i][j] = '~';
+				queue<vector<int>> ch;
+				vector<int> first_vec;
+				first_vec.push_back( i );
+				first_vec.push_back( j );
+				ch.push( first_vec );
+				while( ch.size() ){
+					vector<int> curr_vec = ch.front();
+					if_land( a, n, m, ch, curr_vec, 0 );
+					if_land( a, n, m, ch, curr_vec, 1 );
+					if_land( a, n, m, ch, curr_vec, 2 );
+					if_land( a, n, m, ch, curr_vec, 3 );
+					ch.pop();
+				}
+				count++;
 			}
 
-	try{
-		out<<ans;
-	}
-
-	catch(exception& e){
-		cerr<<e.what();
-		return 1;
-	}
+	out << count;
 
 	in.close();
 	out.close();
