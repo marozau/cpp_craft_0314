0b9372e95049c5057542c5cf9b05a6f2d6de8466
ihar.marozau <ihar.marozau@disfa.com>
2014-03-12 13:22:47 +0300

    added cmake template for 2nd week puzzles

---
+++ solutions/ihar_marozau/2/_gcc_gen.sh
@@ -0,0 +1,22 @@
+
+export SOLUTION_NAME=solutions_s_example_with_test_data_generating
+
+export BOOST_ROOT=/Users/rilley_elf/_dev/cpp_objc/boost_1_47_0
+
+export BUILD_TYPE=Debug
+if [ "$1" == 'Release' ];
+then
+	export BUILD_TYPE=Release
+fi
+
+export BUILD_FOLDER=_build_"$BUILD_TYPE"_32
+
+if [ ! -d $BUILD_FOLDER ];
+then
+	mkdir $BUILD_FOLDER
+fi
+
+cd $BUILD_FOLDER
+cmake -DVERBOSE=ON -DBOOST_STAGE_FOLDER_WITH_ADDRESS_MODEL=ON -DSOLUTION_NAME=$SOLUTION_NAME -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles" ../ 
+cd ../
+
