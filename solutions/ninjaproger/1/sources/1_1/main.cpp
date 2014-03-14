64b60f5d8a33abf1c5b5f376b6d4b76d3b747e06
Alexander Kharevich <alexander.kharevich@viaden.com>
2014-03-14 12:32:48 +0300

    refactored, erase(remove( idiom used, commented code removed

--- solutions/ninjaproger/1/sources/1_1/main.cpp
+++ solutions/ninjaproger/1/sources/1_1/main.cpp
@@ -3,55 +3,41 @@
 #include <string>
 #include <algorithm>
 
-void str_remove_char(std::string *str,const char ch)
-{
-    std::size_t ch_pos = str->find(ch);
-    while(ch_pos!=std::string::npos)
-    {
-        str->erase(ch_pos,1);
-        ch_pos = str->find(ch);
-    }
-}
-
 void str_remove_chars(std::string *str,const std::string str_to_rem)
 {
     for(int i = 0; i < str_to_rem.length(); i++)
-        str_remove_char(str, str_to_rem[i]);
+        str->erase(std::remove(str->begin(),str->end(),str_to_rem[i]), str->end());
 }
-
-
-
-
 
 int main()
 {
-    std::ifstream input_file(SOURCE_DIR "/input.txt");
-    std::ofstream output_file(SOURCE_DIR "/output.txt");
+    std::ifstream in_file(BINARY_DIR "/input.txt");
+    std::ofstream out_file(BINARY_DIR "/output.txt");
     
-    if(!input_file.is_open())
-        return 1;
-    
-    std::string text;
-    std::getline(input_file, text);
-    
-    if(!text.length())
+    if(!in_file.is_open()||!out_file.is_open())
     {
-        std::cout << "Empty file!\n";
+        std::cout << "Couldn't open files!\n";
         return 1;
     }
     
-    std::transform(text.begin(), text.end(),text.begin(), ::tolower);
+    std::string text;
+    std::getline(in_file, text);
     
-    //std::cout << text << std::endl;
+    if(!text.length())
+    {
+        std::cout << "Empty input file!\n";
+        return 1;
+    }
+    
+    std::transform(text.begin(), text.end(),text.begin(),::tolower);
     str_remove_chars(&text, std::string(" \\-"));
-    //std::cout << text << std::endl << std::endl;
     
     std::string keys;
     char delimeters[] = " ";
     
-    while(!input_file.eof())
+    while(!in_file.eof())
     {
-        std::getline(input_file, keys);
+        std::getline(in_file, keys);
         std::transform(keys.begin(), keys.end(),keys.begin(), ::tolower);
         
         std::size_t key_start_pos = 0;
@@ -67,23 +53,14 @@ int main()
             if(key_len)
             {
                 std::string key = keys.substr(key_start_pos,key_len);
-                std::string reversed_key = key;
-                std::reverse(reversed_key.begin(), reversed_key.end());
-
-                std::size_t found_pos = text.find(reversed_key);
+                std::reverse(key.begin(),key.end());
+                std::size_t found_pos = text.find(key);
                 
                 if(found_pos!=std::string::npos)
-                {
-                    output_file << "YES\n";
-                    //std::cout << key << " " << reversed_key << " ["<< found_pos << "]" << " YES\n";
-                }
+                    out_file << "YES\n";
                 else
-                {
-                    output_file << "NO\n";
-                    //std::cout << key << " " << reversed_key << " ["<< found_pos << "]" << " NO\n";
-                }
-                
-                output_file.flush();
+                    out_file << "NO\n";
+
             }
             
             if(next_key_start_pos == std::string::npos)
@@ -93,10 +70,12 @@ int main()
             next_key_start_pos = keys.find_first_of(delimeters,key_start_pos);
         }
         
+        out_file.flush();
+        
     }
     
-    input_file.close();
-    output_file.close();
+    in_file.close();
+    out_file.close();
     
 	return 0;
 }
