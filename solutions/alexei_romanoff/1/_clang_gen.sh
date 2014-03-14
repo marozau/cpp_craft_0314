a761b54ad891bb185926218359b63109bfb1e0d3
Alexei Romanoff <drednout.by@gmail.com>
2014-03-15 00:28:17 +0300

    Added script for generating clang-specific build

---
+++ solutions/alexei_romanoff/1/_clang_gen.sh
@@ -0,0 +1,24 @@
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+export SOLUTION_NAME=puzzle_2_solution
+#if you have custom boost installed, please uncomment
+#this line with proper boost path
+#export BOOST_ROOT=~/usr/boost_1_47_0
+
+export BUILD_TYPE=Debug
+if [ "$1" == 'Release' ];
+then
+	export BUILD_TYPE=Release
+fi
+
+export BUILD_FOLDER=_build_"$BUILD_TYPE"_32
+
+if [ ! -d $BUILD_FOLDER ];
+then
+	mkdir $BUILD_FOLDER
+fi
+
+cd $BUILD_FOLDER
+cmake -DVERBOSE=ON -DBOOST_STAGE_FOLDER_WITH_ADDRESS_MODEL=ON -DSOLUTION_NAME=$SOLUTION_NAME -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles" ../ 
+cd ../
+
