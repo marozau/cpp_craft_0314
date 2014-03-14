eb7a880064842166ce9beae92a40b8a74e0d3281
Irina <ira2060@yandex.ru>
2014-03-14 16:19:38 +0200

    Update 1_3.cpp

--- solutions/IrinaPetrushenko/1/sources/1_3/1_3.cpp
+++ solutions/IrinaPetrushenko/1/sources/1_3/1_3.cpp
@@ -10,36 +10,37 @@ namespace task3{
 	const static char ground='o';
 }
 
-void dfs (int i,size_t j,vector<string> & matrix, const int n, const size_t m){
+void dfs (const int i,const size_t j,vector<string> & matrix, const int n, const size_t m, ofstream & out){
 	matrix[i][j]=task3::water; 
-	if (i+1<n && matrix[i+1][j]==task3::ground) dfs  (i+1,j,matrix,n,m);
-	if (i-1>=0 && matrix[i-1][j]==task3::ground) dfs  (i-1,j,matrix,n,m);
-	if (j+1<m && matrix[i][j+1]==task3::ground) dfs  (i,j+1,matrix,n,m);
-	if (j-1>=0 && matrix[i][j-1]==task3::ground) dfs  (i,j-1,matrix,n,m);
+	if (i+1<n && matrix[i+1][j]==task3::ground) dfs  (i+1,j,matrix,n,m,out);
+	if (i>=1 && matrix[i-1][j]==task3::ground) dfs  (i-1,j,matrix,n,m,out);
+	if (j>=1 && matrix[i][j-1]==task3::ground) dfs  (i,j-1,matrix,n,m,out);
+	if (j+1<m && matrix[i][j+1]==task3::ground) dfs  (i,j+1,matrix,n,m,out);
 }
+
 int main(){
 	ifstream in(BINARY_DIR"/input.txt");
-	ofstream out(BINARY_DIR"/output.txt");
+    ofstream out(BINARY_DIR"/output.txt");
 
  
     if (in.is_open()){
+
 		string str;
 		vector <string> matrix;
-		int n=0;
 		size_t m=0;
+		int n=0;
 
-		int k=0;
 		while (getline (in,str)){
 			m=str.size();
 			matrix.push_back(str);
-			k++;
+			n++;
 		}
-		n=k;
+
 		int ans=0;
 		for (int i=0;i<n;i++)
 			for (size_t j=0;j<m;j++)
 				if (matrix[i][j]==task3::ground){
-					dfs (i,j,matrix,n,m);
+					dfs (i,j,matrix,n,m,out);
 					ans++;
 				}
 
