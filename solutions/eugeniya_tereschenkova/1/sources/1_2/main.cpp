74ab1cee5401b36feb2d6d48b0031198baea60f8
TereschenkovaEugeniya <zhenya.tereschenkova@gmail.com>
2014-03-16 23:02:16 +0300

    edit 1_2 and 1_3

--- solutions/eugeniya_tereschenkova/1/sources/1_2/main.cpp
+++ solutions/eugeniya_tereschenkova/1/sources/1_2/main.cpp
@@ -8,54 +8,52 @@ using namespace std;
 
 namespace Constants_1_2
 {
-	static const char* input_file = BINARY_DIR "/Input.txt";
-	static const char* output_file = BINARY_DIR "/Output.txt";
-	static const string good = "YES";
-	static const string bad  = "NO";
-	static const char* Locale = "Russian";
 	static const int nPrecisionNumbers = 10000;
-	static const int nMaxCodesCount = 1000000;
 }
 
 void get_Codes(ifstream& stream, vector<long long int>& vectCodes)
 {
-	int nCodesCount = 0, nI = 0;
-	double Passw;
+	int nCodesCount = 0;
 
 	stream >> nCodesCount;
 	if(stream.eof())
 		return;
 
-	if(nCodesCount <= 0 || nCodesCount > Constants_1_2::nMaxCodesCount)
+	static const int nMaxCodesCount = 1000000;
+	if(nCodesCount <= 0 || nCodesCount > nMaxCodesCount)
 		return;
 	else
 		vectCodes.reserve(nCodesCount);
 
-	for( nI = 0; nI < nCodesCount && !stream.eof(); nI++) 
+	double Passw;
+	for( int nI = 0; nI < nCodesCount && !stream.eof(); nI++) 
 	{
 		stream >> Passw;
-		vectCodes.push_back((long long int)(Passw*Constants_1_2::nPrecisionNumbers));
+		vectCodes.push_back(static_cast<long long int>(Passw*Constants_1_2::nPrecisionNumbers));
 	}
 	sort(vectCodes.begin(), vectCodes.end());
 }
 
 bool compare_CodesAndPasswords(ifstream& stream, const vector<long long int>& vectCodes) 
 {
-	ofstream output_file( Constants_1_2::output_file );
-	double Passw;
+	static const char* outp_file = BINARY_DIR "/Output.txt";
+	ofstream output_file( outp_file );
 	if(!output_file.is_open())
 	{
 		cout << "Could not open output file" << endl;
 		return false;
 	}
+	double Passw;
+	static const string good = "YES";
+	static const string bad  = "NO";
 	while(!stream.eof())
 	{
 		stream >> Passw;     
 
-		if(binary_search(vectCodes.begin(), vectCodes.end(), (long long int)(Passw*Constants_1_2::nPrecisionNumbers)))
-			output_file << Constants_1_2::good << endl;
+		if(binary_search(vectCodes.begin(), vectCodes.end(), static_cast<long long int>(Passw*Constants_1_2::nPrecisionNumbers)))
+			output_file << good << endl;
 		else 
-			output_file << Constants_1_2::bad << endl;
+			output_file << bad << endl;
 	}
 	output_file.close();
 	return true;
@@ -63,15 +61,17 @@ bool compare_CodesAndPasswords(ifstream& stream, const vector<long long int>& ve
 
 int main()
 {
-	setlocale(LC_ALL, Constants_1_2::Locale);
-	ifstream input_file( Constants_1_2::input_file );
-	vector<long long int> vectCodes;
+	static const char* Locale = "Russian";
+	setlocale(LC_ALL, Locale);
+	static const char* inp_file = BINARY_DIR "/Input.txt";
+	ifstream input_file( inp_file );
 
 	if(!input_file.is_open())
 	{
 		cout << "Could not open input file" << endl;
 		return 1;
 	} 
+	vector<long long int> vectCodes;
 	get_Codes(input_file, vectCodes);
 
 	if(!compare_CodesAndPasswords(input_file, vectCodes))
