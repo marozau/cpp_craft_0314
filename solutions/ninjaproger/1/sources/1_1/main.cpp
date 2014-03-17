7f8fb08358814a9156d184f6eb53f85680de9fae
Alexander Kharevich <alexander.kharevich@viaden.com>
2014-03-17 13:14:18 +0300

    refactored and removed task misunderstandings =)

--- solutions/ninjaproger/1/sources/1_1/main.cpp
+++ solutions/ninjaproger/1/sources/1_1/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <algorithm>
 
-void str_remove_chars(std::string &str,const std::string str_to_rem)
+void str_remove_chars(std::string &str,const std::string &str_to_rem)
 {
     for(int i = 0; i < str_to_rem.length(); i++)
         str.erase(std::remove(str.begin(),str.end(),str_to_rem[i]), str.end());
@@ -32,41 +32,19 @@ int main()
     std::transform(text.begin(), text.end(),text.begin(),::tolower);
     str_remove_chars(text, std::string(" \\-"));
     
-    std::string keys;
-    char delimeters[] = " ";
+    std::string key;
     
-    while(!std::getline(in_file, keys).eof())
+    while(!std::getline(in_file, key).eof())
     {
-        std::transform(keys.begin(), keys.end(),keys.begin(),::tolower);
-        std::size_t key_start_pos = 0;
-        std::size_t next_key_start_pos = keys.find_first_of(delimeters);
-        
-        if(next_key_start_pos==std::string::npos)
-            break;
-        
-        while(1)
-        {
-            std::size_t key_len = next_key_start_pos - key_start_pos;
-            
-            if(key_len)
-            {
-                std::string key = keys.substr(key_start_pos,key_len);
-                std::reverse(key.begin(),key.end());
-                std::size_t found_pos = text.find(key);
+        std::transform(key.begin(), key.end(),key.begin(),::tolower);
+        str_remove_chars(key,std::string(" \\-"));
+        std::reverse(key.begin(), key.end());
+        std::size_t found_pos = text.find(key);
                 
-                if(found_pos!=std::string::npos)
-                    out_file << "YES\n";
-                else
-                    out_file << "NO\n";
-
-            }
-            
-            if(next_key_start_pos == std::string::npos)
-                break;
-            
-            key_start_pos = next_key_start_pos+1;
-            next_key_start_pos = keys.find_first_of(delimeters,key_start_pos);
-        }
+        if(found_pos!=std::string::npos)
+            out_file << "YES\n";
+        else
+            out_file << "NO\n";
         
         out_file.flush();
         
