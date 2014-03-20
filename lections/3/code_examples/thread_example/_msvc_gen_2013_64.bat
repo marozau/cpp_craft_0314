032ab570cc55eec7d702a3be8a2ba7c6bf55b3cb
Igor Kolupaev <ikolupaev@gmail.com>
2014-03-21 00:15:29 +0300

    ...

---
+++ lections/3/code_examples/thread_example/_msvc_gen_2013_64.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set BOOST_ROOT=d:\usr\boost_1_47_0
+set SOLUTION_NAME=threads
+
+set BUILD_TYPE=Debug
+if [%1]==[Release] (
+	set BUILD_TYPE=Release
+)
+set BUILD_FOLDER=_build_%BUILD_TYPE%_64
+
+if not exist %BUILD_FOLDER% (
+	mkdir %BUILD_FOLDER%
+)
+
+cd %BUILD_FOLDER%
+cmake -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=%SOLUTION_NAME% -G "Visual Studio 12 Win64" ../ 
+cd ../
+
+echo "%BUILD_FOLDER%/%SOLUTION_NAME%.sln" > _start_msvc.bat
