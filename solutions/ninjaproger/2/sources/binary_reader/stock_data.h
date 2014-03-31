4624e63155a352be8dfd99ade7c06e2e2873464e
Alexander Kharevich <ninjaproger@Alexanders-Mac-Pro.local>
2014-03-31 20:05:38 +0300

    refactored with guru’s corrections

--- solutions/ninjaproger/2/sources/binary_reader/stock_data.h
+++ solutions/ninjaproger/2/sources/binary_reader/stock_data.h
@@ -35,8 +35,8 @@ namespace binary_reader
 							const double f4 );		
 		~stock_data();
 		//
-		void write( std::ofstream& out );
-		void write_raw( std::ofstream& out );
+		void write( std::ofstream& out ) const;
+		void write_raw( std::ofstream& out ) const;
 	};
 }
 
