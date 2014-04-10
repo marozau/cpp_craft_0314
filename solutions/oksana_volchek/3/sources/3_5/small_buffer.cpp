8d125bd377eb81cee9b800368b487025cd47316b
Oksana Volchek <VolchekOA@gmail.com>
2014-04-10 22:57:36 +0200

    Cleaning up homework 3

--- solutions/oksana_volchek/3/sources/3_5/small_buffer.cpp
+++ solutions/oksana_volchek/3/sources/3_5/small_buffer.cpp
@@ -15,22 +15,18 @@ namespace myConstants {
 	const boost::regex templateName(".+input_(\\d){3}\\.txt$");
 }
 
-namespace threadManagement {
-	boost::mutex myMutex;
-}
-
 typedef pair<boost::uint32_t, boost::uint32_t> pairValues;
 typedef map<boost::uint32_t, pairValues> timeStorage;
 typedef map<boost::uint32_t, timeStorage> typeStorage;
-typedef map<boost::uint32_t, map<boost::uint32_t, boost::uint32_t>> fullRes;
+
 
 const boost::uint32_t msgSize(const binary_reader::market_message& myClass){
 	return sizeof(myClass.type()) + sizeof(myClass.time()) + sizeof(myClass.len()) + myClass.len();
 }
 
-int saveMsg(string const& inputFile, fullRes& myMap){
+int getSaveMsg(string const& inputFile, string const& outputFile){
 	ifstream filein;
-	filein.open(inputFile, ios::binary);
+	filein.open(BINARY_DIR "/" + inputFile, ios::binary);
 	if(!filein.is_open()){
 		cerr << "Unable to open file input.txt!" << endl;
 		exit(1);
@@ -39,29 +35,23 @@ int saveMsg(string const& inputFile, fullRes& myMap){
 	boost::uint32_t currentTime;
 	boost::uint32_t currentType;
 	typeStorage allTypes;
+
 	try {
 		while (!filein.eof()){
 			binary_reader::market_message market(filein);
 			if (filein.good() && msgSize(market) <= myConstants::maxSize){
 				currentTime = market.time();
 				currentType = market.type();
-				boost::mutex::scoped_lock lock(threadManagement::myMutex);
-				// cout << currentType << "\t"  << currentTime << "\t" << market.len() << endl;
 				if (!allTypes.count(currentType)){
 					if (!allTypes[currentType].count(currentTime) && msgSize(market) <= myConstants::maxSize){
 						allTypes[currentType] = timeStorage();
 						allTypes[currentType][currentTime] = make_pair(1, msgSize(market));
-						if (!myMap.count(currentType)){
-							myMap[currentType][currentTime] = 1;
-						}
-						else { ++myMap[currentType][currentTime]; }
 					}
 				}
 				else if (allTypes[currentType][currentTime].second + msgSize(market) <= myConstants::maxSize){
 					++allTypes[currentType][currentTime].first;
 					allTypes[currentType][currentTime].second += msgSize(market);
-					++myMap[currentType][currentTime];
-				}
+				}	
 			}
 		}
 	}
@@ -69,6 +59,25 @@ int saveMsg(string const& inputFile, fullRes& myMap){
 		cerr << "Error!!!" << endl;
 	}
 	filein.close();
+
+	ofstream fileout;
+	fileout.open(BINARY_DIR "/" + outputFile, ios::binary);
+	double numerator;
+	for (typeStorage::iterator iter = allTypes.begin(); 
+	   iter != allTypes.end(); ++iter){
+		numerator = 0;
+		currentType = iter->first;
+		timeStorage inner_map = iter->second;
+		for (timeStorage::iterator inner_iter = inner_map.begin(); 
+			inner_iter != inner_map.end(); ++inner_iter){
+			numerator += (inner_iter->second).first;
+		}
+		const double average = numerator / inner_map.size();
+//		cout << currentType << "\t" << average << endl;
+		fileout.write(reinterpret_cast< char* >(&currentType), sizeof(currentType));
+		fileout.write(reinterpret_cast< const char* >(&average), sizeof(average));
+	}
+	fileout.close();
 
 	return 0;
 }
@@ -79,33 +88,16 @@ int main()
 	const boost::filesystem::directory_iterator end;
 	boost::filesystem::path name;
 	boost::thread_group myThreads;
-	fullRes allRes;
 	for (boost::filesystem::directory_iterator it(fullPath); it != end; ++it) {
 		name = *it;
 		if (boost::regex_match(name.string(), myConstants::templateName)){
-			const string thisInputFile = BINARY_DIR "/" + name.filename().string();
-			myThreads.create_thread(boost::bind(&saveMsg, thisInputFile, boost::ref(allRes)));
+			const string thisInputFile = name.filename().string();
+			string tmp = thisInputFile;
+			const string thisOutputFile = tmp.replace(0, 2, "out");
+			myThreads.create_thread(boost::bind(&getSaveMsg, thisInputFile, thisOutputFile));
 		}
 	}
 	myThreads.join_all();
-
-	ofstream fileout;
-	fileout.open(BINARY_DIR "/output.txt", ios::binary);
-	double numerator;
-	for (fullRes::iterator iter = allRes.begin(); iter != allRes.end(); ++iter){
-		numerator = 0;
-		boost::uint32_t currentType = iter->first;
-		map<boost::uint32_t, boost::uint32_t> inner_map = iter->second;
-		for (map<boost::uint32_t, boost::uint32_t>::iterator inner_iter = inner_map.begin(); 
-			inner_iter != inner_map.end(); ++inner_iter){
-			numerator += inner_iter->second;
-		}
-		const double average = numerator / inner_map.size();
-		// cout << currentType << "\t" << average << endl;
-		fileout.write(reinterpret_cast< char* >(&currentType), sizeof(currentType));
-		fileout.write(reinterpret_cast< const char* >(&average), sizeof(average));
-	}
-	fileout.close();
 
 	return 0;
 }
--- solutions/oksana_volchek/3/sources/3_6/binary_datafeed.cpp
+++ solutions/oksana_volchek/3/sources/3_6/binary_datafeed.cpp
@@ -3,7 +3,6 @@
 #include <map>
 #include <boost/lexical_cast.hpp>
 #include <boost/thread.hpp>
-#include <boost/asio.hpp>
 //#include <boost/timer/timer.hpp>
 #include "stock_data.h"
 
@@ -12,13 +11,31 @@ using namespace std;
 namespace threadManagement
 {
 	boost::mutex myMutex;
-	static const size_t threadsCount = 10;
+	static const size_t threadsCount = 4;
 }
 
-void writeMsg(ofstream* fileout, binary_reader::stock_data* currentMsg){
+typedef map<const string, ofstream*> ofstreamInfo;
+
+void writeSortMsg(ifstream & myFilein, ofstreamInfo & myStockNameMap){
 	boost::mutex::scoped_lock lock(threadManagement::myMutex);
-	currentMsg->write(*fileout);
-	delete currentMsg;
+	while (!myFilein.eof()){
+		binary_reader::stock_data *info = new binary_reader::stock_data(myFilein);
+		if (myFilein.good()){
+			try {
+				const string nameFileout = BINARY_DIR "/output_" + boost::lexical_cast<string>(info->get_stockName()) + ".txt";
+				if (!myStockNameMap[nameFileout]){
+					ofstream* thisFileout = new ofstream(nameFileout, ios::binary);
+					myStockNameMap[nameFileout] = thisFileout;
+				}
+				
+				info->write(*myStockNameMap[nameFileout]);
+				delete info;
+			}
+			catch (...){
+				cerr << "Unable to employ lexical_cast!" << endl;
+			}
+		}
+	}
 }
 
 int main()
@@ -30,37 +47,20 @@ int main()
 		return 1;
 	}
 
-	map<const string, ofstream*> stockNamesAttested;
-	boost::asio::io_service myService;
-	boost::asio::io_service::work myWork(myService);
+	ofstreamInfo stockNamesAttested;
 	boost::thread_group myThreads;
-	for (size_t i = 0; i < threadManagement::threadsCount; ++i){
-	    myThreads.create_thread(boost::bind(&boost::asio::io_service::run, &myService));
-	}
-
 	// boost::timer::auto_cpu_timer t;
 
-	while (!filein.eof()){
-		binary_reader::stock_data *info = new binary_reader::stock_data(filein);
-		if (filein.good()){
-			try {
-				const string nameFileout = BINARY_DIR "/output_" + boost::lexical_cast<string>(info->get_stockName()) + ".txt";
-				if (!stockNamesAttested[nameFileout]){
-					ofstream* thisFileout = new ofstream(nameFileout, ios::binary);
-					stockNamesAttested[nameFileout] = thisFileout;
-				}
-				myService.post(boost::bind(&writeMsg, stockNamesAttested[nameFileout], info));
-			}
-			catch (...){
-				cerr << "Unable to employ lexical_cast!" << endl;
-			}
-		}
+	for (size_t i = 0; i < threadManagement::threadsCount; ++i){
+        myThreads.create_thread(boost::bind(&writeSortMsg, boost::ref(filein), boost::ref(stockNamesAttested)));
 	}
-	myService.stop();
+
 	myThreads.join_all();
 	filein.close();
-	for (map<const string, ofstream*>::iterator iter = stockNamesAttested.begin(); iter != stockNamesAttested.end(); ++iter){
+	
+	for (ofstreamInfo::iterator iter = stockNamesAttested.begin(); iter != stockNamesAttested.end(); ++iter){
 		(iter->second)->close();
 	}
+
 	return 0;
 }
