4e98dce01250f207384952e190ba55cafea52726
TereschenkovaEugeniya <zhenya.tereschenkova@gmail.com>
2014-04-22 15:51:39 +0300

    Update solution.h

--- solutions/eugeniya_tereschenkova/4/sources/4_5/solution.h
+++ solutions/eugeniya_tereschenkova/4/sources/4_5/solution.h
@@ -2,13 +2,24 @@
 #define _TASK4_5_SOLUTION_H_
 
 #include <vector>
+#include <boost\thread.hpp>
 
 namespace task4_5
 {
-	typedef std::vector< std::vector< int > > data_type;
+	typedef std::vector< int > cont_type;
+	typedef std::vector< cont_type > data_type;
+	typedef cont_type::const_iterator cont_const_it;
 
 	class solution
 	{
+	private:
+		boost::mutex wait_min_max_mutex_;
+		
+		int min_;
+		int max_;
+		
+		void find_min_max(cont_type& cont);
+	
 	public:
 		explicit solution( const data_type& data );
 		int get_min() const;
@@ -17,4 +28,6 @@ namespace task4_5
 }
 
 #endif // _TASK4_5_SOLUTION_H_
+
+
 
