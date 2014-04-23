1837bb6cdb04b51f9796ac28166ec0644ece1ce7
dasha-popova <lady.pops@yandex.ru>
2014-04-23 18:54:51 +0300

    week 3 =)

--- solutions/ihar_marozau/2/_msvc_gen_2010_64.bat
+++ solutions/ihar_marozau/2/_msvc_gen_2010_64.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set SOLUTION_NAME=puzzle_2_solution
-set BOOST_ROOT=d:/usr/boost_1_47_0
+set BOOST_ROOT=C:\Boost
 
 set BUILD_TYPE=Debug
 if [%1]==[Release] (
@@ -14,7 +14,7 @@ if not exist %BUILD_FOLDER% (
 )
 
 cd %BUILD_FOLDER%
-cmake -DBOOST_STAGE_FOLDER_WITH_ADDRESS_MODEL=ON -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=%SOLUTION_NAME%  -G "Visual Studio 10 Win64" ../ 
+cmake -DBOOST_STAGE_FOLDER_WITH_ADDRESS_MODEL=ON -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=%SOLUTION_NAME%  -G "Visual Studio 11" ../ 
 cd ../
 
 echo "%BUILD_FOLDER%/%SOLUTION_NAME%.sln" > _start_msvc.bat
