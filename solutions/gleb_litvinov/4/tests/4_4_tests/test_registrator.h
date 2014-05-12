021bfdc2829d03f5530e8e146196c298949d6d27
Litvinov_Gleb <fett2008@yandex.ru>
2014-05-12 14:07:19 +0300

    task4

---
+++ solutions/gleb_litvinov/4/tests/4_4_tests/test_registrator.h
@@ -0,0 +1,15 @@
+#include <boost/test/test_tools.hpp>
+
+namespace task4_4
+{
+	namespace tests_
+	{
+		void message_constructor_tests();
+
+		void a_message_constructor_tests();
+		void b_message_constructor_tests();
+
+		void message_reader_constructor_tests();
+		void message_reader_read_file_tests();
+	}
+}
